e17b8115f852f1a348503405e517e008a908ea49
Zaid <itsunknown122@gmail.com>
2022-06-23 22:10:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+APP_ID = int(os.environ.get("APP_ID", "6435225"))
+API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+BOT_TOKEN = os.environ.get("TOKEN", "5149950951:AAGrA23C2cvlOvH81ulmsCgjC_c8oflx7HE")
