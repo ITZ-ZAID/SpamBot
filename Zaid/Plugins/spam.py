0b4e064935fc7a48608a8ad2bec9b56ee540a698
Zaid <itsunknown122@gmail.com>
2022-06-23 22:46:08 +0530

    Update spam.py

--- Zaid/Plugins/spam.py
+++ Zaid/Plugins/spam.py
@@ -4,38 +4,20 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, User
 
 
-def ReplyCheck(message: Message):
-    reply_id = None
+@Client.on_message(filters.command('spam'))
+async def spam(client: Client, message: Message):
+    quantity = message.command[1]
+    spam_text = ' '.join(message.command[2:])
+    quantity = int(quantity)
 
     if message.reply_to_message:
-        reply_id = message.reply_to_message.id
+        reply_to_id = message.reply_to_message.message_id
+        for _ in range(quantity):
+            await client.send_message(message.chat.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.15)
+        return
 
-    elif not message.from_user.is_self:
-        reply_id = message.id
-
-    return reply_id
-
-
-
-@Client.on_message(filters.command("spam", "!"))
-async def spam(bot: Client, message: Message):
-    # Get current chat and spam to there.
-    # if in group and replied to user, then spam replying to user.
-    await message.delete()
-
-    times = message.command[1]
-    to_spam = " ".join(message.command[2:])
-
-    if message.chat.type in ["supergroup", "group"]:
-        for _ in range(int(times)):
-            await bot.send_message(
-                message.chat.id, to_spam, reply_to_message_id=ReplyCheck(message)
-            )
-            await asyncio.sleep(0.20)
-
-    if message.chat.type == "private":
-        for _ in range(int(times)):
-            await bot.send_message(message.chat.id, to_spam)
-            await asyncio.sleep(0.20)
-
-
+    for _ in range(quantity):
+        await client.send_message(message.chat.id, spam_text)
+        await asyncio.sleep(0.15)
