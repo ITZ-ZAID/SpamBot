90c7a79baaecd7dd45cdda326b0f86713dc7b691
Zaid <itzzaid@pm.me>
2022-06-23 22:54:06 +0530

    Update spam.py

--- Zaid/Plugins/spam.py
+++ Zaid/Plugins/spam.py
@@ -15,9 +15,9 @@ async def spam(client: Client, message: Message):
         for _ in range(quantity):
             await client.send_message(message.chat.id, spam_text,
                                       reply_to_message_id=reply_to_id)
-            await asyncio.sleep(0.000001)
+            await asyncio.sleep(0.1)
         return
 
     for _ in range(quantity):
         await client.send_message(message.chat.id, spam_text)
-        await asyncio.sleep(0.0000001)
+        await asyncio.sleep(0.1)
