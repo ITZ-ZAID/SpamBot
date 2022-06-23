35729df4e5bcfd55581ff03fc51d7acfa6d52b01
Zaid <itsunknown122@gmail.com>
2022-06-23 22:25:01 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -1,4 +1,4 @@
 from pyrogram import Client, filters
 from config import *
 
-bot = Client("memory", plugins=dict(root="Zaid.Plugins"), bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+bot = Client("memory", plugins=dict(root=f"Zaid.Plugins"), bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
