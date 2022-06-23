a101a89ef1dc709b89ee30e431d1b58e0857b02d
Zaid <itzzaid@pm.me>
2022-06-23 23:02:59 +0530

    Update spam.py

--- Zaid/Plugins/spam.py
+++ Zaid/Plugins/spam.py
@@ -8,9 +8,8 @@ from pyrogram.types import Message, User
 async def spam(client: Client, message: Message):
     spam_text = ' '.join(message.command[2:])
     if not spam_text:
-        await message.reply_text("Usage: \n\n/spam <count> message")
+        await message.reply_text("Usage: \n\n/spam {Count} message")
     quantity = message.command[1]
-    spam_text = ' '.join(message.command[2:])
     quantity = int(quantity)
 
     if message.reply_to_message:
