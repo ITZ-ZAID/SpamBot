b3084dce9eebcd6594f5fd7eab7768ca8e2ddd60
Zaid <itsunknown122@gmail.com>
2022-06-23 22:35:52 +0530

    Create spam.py

---
+++ Zaid/Plugins/spam.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+
+
+@Client.on_message(filters.command("spam", ".") & filters.me)
+async def spam(bot: Client, message: Message):
+    # Get current chat and spam to there.
+    # if in group and replied to user, then spam replying to user.
+    await message.delete()
+
+    times = message.command[1]
+    to_spam = " ".join(message.command[2:])
+
+    if message.chat.type in ["supergroup", "group"]:
+        for _ in range(int(times)):
+            await bot.send_message(
+                message.chat.id, to_spam, reply_to_message_id=ReplyCheck(message)
+            )
+            await asyncio.sleep(0.20)
+
+    if message.chat.type == "private":
+        for _ in range(int(times)):
+            await bot.send_message(message.chat.id, to_spam)
+            await asyncio.sleep(0.20)
+
+
