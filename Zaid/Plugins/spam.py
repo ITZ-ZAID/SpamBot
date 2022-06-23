2156d17ba306db88a85be542270173b11cd61ee0
Zaid <itsunknown122@gmail.com>
2022-06-23 22:37:11 +0530

    Update spam.py

--- Zaid/Plugins/spam.py
+++ Zaid/Plugins/spam.py
@@ -1,7 +1,19 @@
 import asyncio
 
 from pyrogram import filters, Client
-from pyrogram.types import Message
+from pyrogram.types import Message, User
+
+
+def ReplyCheck(message: Message):
+    reply_id = None
+
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.id
+
+    elif not message.from_user.is_self:
+        reply_id = message.id
+
+    return reply_id
 
 
 
